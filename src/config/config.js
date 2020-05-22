b332c3aaa57d7eb75ebcd09fc130052e9f2f4030
WGolon <60260383+WGolon@users.noreply.github.com>
2020-05-22 16:28:37 +0200

    Delete config.js

--- src/config/config.js
+++
@@ -1,8 +0,0 @@
-const acc = {
-    uname: 'test',
-    pwd: 'test',
-}
-
-export const secretKey = `LdPQgZMB5wuet8q`;
-
-export const mongoURI = `mongodb+srv://${acc.uname}:${acc.pwd}@nodetodosample-cmtew.mongodb.net/test?retryWrites=true&w=majority`;
